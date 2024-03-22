f786c104cf8ae83ea2eda4950ac3f049de1d3148
Camilo <camilo.muno27@correo.tdea.edu.co>
2024-03-21 22:52:24 -0500

    Agregan modificaciones para exportaciones e importaciones según ES6

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const { envs } = require('./config/env');
-const { startServer } = require('./server/server')
+import { envs } from './config/env.js';
+import { startServer } from './server/server.js'
 
-main = async() => {
+const main = async() => {
     startServer({
         port : envs.PORT,
         public_path : envs.PUBLIC_PATH
